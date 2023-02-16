1a568acda76938d03374d14c95a7e449f9ff4817
Artyom <berezoviu@list.ru>
2023-02-16 15:49:12 +0300

    push1

---
+++ Less 1/Lesson1/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = int.Parse(Console.ReadLine());
+//Console.WriteLine("Вы ввели число - ");
+//Console.WriteLine(number);
+
+// Возведение в квадрат 
+int result = number * number;
+Console.WriteLine(result);
+
+
+
+
